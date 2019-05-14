15295bb8557110438bee9e1c2583f37cd82d22a5
Dimas Firmansyah <dimasfirmansyah@yandex.com>
2019-05-13 20:15:22 -0700

    change connection

--- core/connection.php
+++ core/connection.php
@@ -1,13 +1,13 @@
 <?php 
 
-// $host = '153.92.11.35';
-// $user = 'u6593255_root';
-// $pass = '12131265@dimas';
-// $db   = 'u6593255_marketplace';
+$hostDB 	= '153.92.11.35';
+$userDB 	= 'u6593255_root';
+$passDB 	= '12131265@dimas';
+$selectDB   = 'u6593255_marketplace';
 
-$hostDB   = 'localhost';
-$userDB   = 'root';
-$passDB   = '';
-$selectDB = 'marketplace';
+// $hostDB   = 'localhost';
+// $userDB   = 'root';
+// $passDB   = '';
+// $selectDB = 'marketplace';
 
 $conn = mysqli_connect($hostDB,$userDB,$passDB,$selectDB);
